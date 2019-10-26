f49a2a1e6b2a6c6199188d39cd1790d7af53c5fd
bobby.li <lihan0603@hupu.com>
2019-10-26 23:33:51 +0800

    [omni-door]: 0.0.5-alpha.12

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -2,18 +2,22 @@ import shelljs from 'shelljs';
 import { logErr, logInfo, logWarn } from '../utils/logger';
 import { OmniConfig } from '../index.d';
 
-export default function (config: OmniConfig | {}, {
-  fc,
-  cc
-}: {
+export default function (config: OmniConfig | {}, params?: {
   fc?: boolean;
   cc?: boolean;
 }) {
+  if (!params) {
+    logErr('Please input your component name!');
+    return;
+  }
+
   if (JSON.stringify(config) === '{}') {
     logWarn('Please Initialize project first');
     shelljs.exec('omni init');
     return;
   }
+
+  const { fc, cc } = params!;
 
   logInfo(JSON.stringify(config));
 }
