cc7b6e4c3b6e5c67d20362fd4883e7f5f1c6dc6a
bobby.li <bobby.lhan@gmail.com>
2020-02-27 16:56:02 +0800

    utils logo test

--- src/utils/__test__/index.test.ts
+++ src/utils/__test__/index.test.ts
@@ -1,10 +1,17 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { getHandlers, handlerFactory } from '../tackle_plugins';
+import logo from '../logo';
 
 describe('tackle_plugins test', function () {
   it('type checking', function () {
     expect(getHandlers).to.be.a('function');
     expect(handlerFactory).to.be.a('function');
+  });
+});
+
+describe('logo test', function () {
+  it('type checking', function () {
+    expect(logo).to.be.a('string');
   });
 });
