64d3c3baf8bb44f59bc9da345276a0433650df01
bobby.li <bobby.lhan@gmail.com>
2020-04-15 18:05:07 +0800

    🐸  [OMNI-DOOR]: 1.2.17

--- .build/publish.sh
+++ .build/publish.sh
@@ -9,7 +9,7 @@ then
   git add -A
   git commit -m "${name}: ${version}"
   git push
-  npm publish
+  npm publish --registry='https://registry.npmjs.org'
   echo -e "\033[32m \n${name}:publish success: ${version}\n \033[0m"
 else
   echo -e "\033[31m \n${name}:publish failed: ${version}\n \033[0m"
