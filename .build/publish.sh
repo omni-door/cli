6bfd2785904a1f4769cdf5858d47b0827371f1d6
bobby.li <lihan0603@hupu.com>
2020-07-22 15:42:03 +0800

    build script msg modify

--- .build/publish.sh
+++ .build/publish.sh
@@ -10,7 +10,7 @@ then
   git commit -m "${name}: ${version}"
   git push
   npm publish --registry='https://registry.npmjs.org'
-  echo -e "\033[32m \n${name}: Publish success - ${version}\n \033[0m"
+  echo -e "\033[32m \n${name}: The npm-package publish success - ${version}\n \033[0m"
 else
-  echo -e "\033[31m \n${name}: Publish failed - ${version}\n \033[0m"
+  echo -e "\033[31m \n${name}: The npm-package publish failed!\n \033[0m"
 fi
--- .build/version.sh
+++ .build/version.sh
@@ -22,19 +22,19 @@ updateVersion () {
   manualVersion=$(echo "$iterate" | grep [0-9]\.[0-9]\.[0-9])
   if [ "$iterate" = "i" -o "$iterate" = "ignore" ]
   then
-    echo -e "\033[33m${name}: Ignoring version iteration\033[0m"
+    echo -e "\033[33m${name}: Ignoring the version of iteration\033[0m"
   elif [ -z "$iterate" ]
   then
     newSubSubVersion=`expr $subSubVersion + 1`
     newVersion=$(echo ${version/${dot}${subVersion}${dot}${subSubVersion}/${dot}${subVersion}${dot}${newSubSubVersion}})
     newVersionLine=$(echo "${versionLine/${version}/${newVersion}}")
-    echo -e "\033[36m${name}: Auto version iteration to ${newVersion}\033[0m"
+    echo -e "\033[36m${name}: Auto-increase the version of iteration to ${newVersion}\033[0m"
     replaceVersion "$versionLine" "$newVersionLine"
   elif [ -n "$manualVersion" ]
     then
     newVersion=$(echo ${version/${version}/${manualVersion}})
     newVersionLine=$(echo "${versionLine/${version}/${newVersion}}")
-    echo -e "\033[35m${name}: Manual version iteration to ${manualVersion}\033[0m"
+    echo -e "\033[35m${name}: Manual specify the version of iteration to ${manualVersion}\033[0m"
     replaceVersion "$versionLine" "$newVersionLine"
   else
     echo -e "\033[41;37m${name}: Please input correct version number\033[0m"
