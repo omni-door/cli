9e44b59caf82ea7d6c817f096db7250a105234dd
bobby.li <lihan0603@hupu.com>
2020-08-04 19:24:07 +0800

    test

--- src/commands/initial/__test__/index.test.ts
+++ src/commands/initial/__test__/index.test.ts
@@ -2,11 +2,12 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import initial from '../index';
 import {
-  cli_basic,
-  cli_standard,
-  cli_entire,
-  cli_lib_components,
-  cli_lib_toolkit
+  cli_basic_react,
+  cli_standard_react,
+  cli_entire_react,
+  cli_ssr_react,
+  cli_components_react,
+  cli_toolkit
 } from '../initial_preset';
 
 
@@ -17,161 +18,161 @@ describe('initial command test', function () {
 });
 
 describe('initial preset test', function () {
-  it('cli_basic checking', function () {
-    expect(cli_basic).to.be.an('object');
+  it('cli_basic_react checking', function () {
+    expect(cli_basic_react).to.be.an('object');
 
-    expect(cli_basic.build).to.be.a('string');
-    expect(cli_basic.build).to.be.equal('webpack');
+    expect(cli_basic_react.build).to.be.a('string');
+    expect(cli_basic_react.build).to.be.equal('webpack');
 
-    expect(cli_basic.pkgtool).to.be.a('string');
-    expect(cli_basic.pkgtool).to.be.equal('yarn');
+    expect(cli_basic_react.pkgtool).to.be.a('string');
+    expect(cli_basic_react.pkgtool).to.be.equal('yarn');
 
-    expect(cli_basic['project_type']).to.be.a('string');
-    expect(cli_basic['project_type']).to.be.equal('spa-react');
+    expect(cli_basic_react['project_type']).to.be.a('string');
+    expect(cli_basic_react['project_type']).to.be.equal('spa-react');
 
-    expect(cli_basic.ts).to.be.a('boolean');
-    expect(cli_basic.ts).to.be.false;
+    expect(cli_basic_react.ts).to.be.a('boolean');
+    expect(cli_basic_react.ts).to.be.false;
 
-    expect(cli_basic.testFrame).to.be.a('string');
-    expect(cli_basic.testFrame).to.be.equal('');
+    expect(cli_basic_react.testFrame).to.be.a('string');
+    expect(cli_basic_react.testFrame).to.be.equal('');
 
-    expect(cli_basic.eslint).to.be.a('boolean');
-    expect(cli_basic.eslint).to.be.false;
+    expect(cli_basic_react.eslint).to.be.a('boolean');
+    expect(cli_basic_react.eslint).to.be.false;
 
-    expect(cli_basic.commitlint).to.be.a('boolean');
-    expect(cli_basic.commitlint).to.be.false;
+    expect(cli_basic_react.commitlint).to.be.a('boolean');
+    expect(cli_basic_react.commitlint).to.be.false;
 
-    expect(cli_basic.style).to.be.a('string');
-    expect(cli_basic.style).to.be.equal('css');
+    expect(cli_basic_react.style).to.be.a('string');
+    expect(cli_basic_react.style).to.be.equal('css');
 
-    expect(cli_basic.stylelint).to.be.a('boolean');
-    expect(cli_basic.stylelint).to.be.false;
+    expect(cli_basic_react.stylelint).to.be.a('boolean');
+    expect(cli_basic_react.stylelint).to.be.false;
   });
 
-  it('cli_standard checking', function () {
-    expect(cli_standard).to.be.an('object');
+  it('cli_standard_react checking', function () {
+    expect(cli_standard_react).to.be.an('object');
 
-    expect(cli_standard.build).to.be.a('string');
-    expect(cli_standard.build).to.be.equal('webpack');
+    expect(cli_standard_react.build).to.be.a('string');
+    expect(cli_standard_react.build).to.be.equal('webpack');
 
-    expect(cli_standard.pkgtool).to.be.a('string');
-    expect(cli_standard.pkgtool).to.be.equal('yarn');
+    expect(cli_standard_react.pkgtool).to.be.a('string');
+    expect(cli_standard_react.pkgtool).to.be.equal('yarn');
 
-    expect(cli_standard['project_type']).to.be.a('string');
-    expect(cli_standard['project_type']).to.be.equal('spa-react');
+    expect(cli_standard_react['project_type']).to.be.a('string');
+    expect(cli_standard_react['project_type']).to.be.equal('spa-react');
 
-    expect(cli_standard.ts).to.be.a('boolean');
-    expect(cli_standard.ts).to.be.true;
+    expect(cli_standard_react.ts).to.be.a('boolean');
+    expect(cli_standard_react.ts).to.be.true;
 
-    expect(cli_standard.testFrame).to.be.a('string');
-    expect(cli_standard.testFrame).to.be.equal('');
+    expect(cli_standard_react.testFrame).to.be.a('string');
+    expect(cli_standard_react.testFrame).to.be.equal('');
 
-    expect(cli_standard.eslint).to.be.a('boolean');
-    expect(cli_standard.eslint).to.be.true;
+    expect(cli_standard_react.eslint).to.be.a('boolean');
+    expect(cli_standard_react.eslint).to.be.true;
 
-    expect(cli_standard.commitlint).to.be.a('boolean');
-    expect(cli_standard.commitlint).to.be.false;
+    expect(cli_standard_react.commitlint).to.be.a('boolean');
+    expect(cli_standard_react.commitlint).to.be.false;
 
-    expect(cli_standard.style).to.be.a('string');
-    expect(cli_standard.style).to.be.equal('less');
+    expect(cli_standard_react.style).to.be.a('string');
+    expect(cli_standard_react.style).to.be.equal('less');
 
-    expect(cli_standard.stylelint).to.be.a('boolean');
-    expect(cli_standard.stylelint).to.be.true;
+    expect(cli_standard_react.stylelint).to.be.a('boolean');
+    expect(cli_standard_react.stylelint).to.be.true;
   });
 
-  it('cli_entire checking', function () {
-    expect(cli_entire).to.be.an('object');
+  it('cli_entire_react checking', function () {
+    expect(cli_entire_react).to.be.an('object');
 
-    expect(cli_entire.build).to.be.a('string');
-    expect(cli_entire.build).to.be.equal('webpack');
+    expect(cli_entire_react.build).to.be.a('string');
+    expect(cli_entire_react.build).to.be.equal('webpack');
 
-    expect(cli_entire.pkgtool).to.be.a('string');
-    expect(cli_entire.pkgtool).to.be.equal('yarn');
+    expect(cli_entire_react.pkgtool).to.be.a('string');
+    expect(cli_entire_react.pkgtool).to.be.equal('yarn');
 
-    expect(cli_entire['project_type']).to.be.a('string');
-    expect(cli_entire['project_type']).to.be.equal('spa-react');
+    expect(cli_entire_react['project_type']).to.be.a('string');
+    expect(cli_entire_react['project_type']).to.be.equal('spa-react');
 
-    expect(cli_entire.ts).to.be.a('boolean');
-    expect(cli_entire.ts).to.be.true;
+    expect(cli_entire_react.ts).to.be.a('boolean');
+    expect(cli_entire_react.ts).to.be.true;
 
-    expect(cli_entire.testFrame).to.be.a('string');
-    expect(cli_entire.testFrame).to.be.equal('jest');
+    expect(cli_entire_react.testFrame).to.be.a('string');
+    expect(cli_entire_react.testFrame).to.be.equal('jest');
 
-    expect(cli_entire.eslint).to.be.a('boolean');
-    expect(cli_entire.eslint).to.be.true;
+    expect(cli_entire_react.eslint).to.be.a('boolean');
+    expect(cli_entire_react.eslint).to.be.true;
 
-    expect(cli_entire.commitlint).to.be.a('boolean');
-    expect(cli_entire.commitlint).to.be.true;
+    expect(cli_entire_react.commitlint).to.be.a('boolean');
+    expect(cli_entire_react.commitlint).to.be.true;
 
-    expect(cli_entire.style).to.be.a('string');
-    expect(cli_entire.style).to.be.equal('all');
+    expect(cli_entire_react.style).to.be.a('string');
+    expect(cli_entire_react.style).to.be.equal('all');
 
-    expect(cli_entire.stylelint).to.be.a('boolean');
-    expect(cli_entire.stylelint).to.be.true;
+    expect(cli_entire_react.stylelint).to.be.a('boolean');
+    expect(cli_entire_react.stylelint).to.be.true;
   });
 
-  it('cli_lib_components checking', function () {
-    expect(cli_lib_components).to.be.an('object');
+  it('cli_components_react checking', function () {
+    expect(cli_components_react).to.be.an('object');
 
-    expect(cli_lib_components.build).to.be.a('string');
-    expect(cli_lib_components.build).to.be.equal('tsc');
+    expect(cli_components_react.build).to.be.a('string');
+    expect(cli_components_react.build).to.be.equal('tsc');
 
-    expect(cli_lib_components.pkgtool).to.be.a('string');
-    expect(cli_lib_components.pkgtool).to.be.equal('yarn');
+    expect(cli_components_react.pkgtool).to.be.a('string');
+    expect(cli_components_react.pkgtool).to.be.equal('yarn');
 
-    expect(cli_lib_components['project_type']).to.be.a('string');
-    expect(cli_lib_components['project_type']).to.be.equal('component-react');
+    expect(cli_components_react['project_type']).to.be.a('string');
+    expect(cli_components_react['project_type']).to.be.equal('component-react');
 
-    expect(cli_lib_components.ts).to.be.a('boolean');
-    expect(cli_lib_components.ts).to.be.true;
+    expect(cli_components_react.ts).to.be.a('boolean');
+    expect(cli_components_react.ts).to.be.true;
 
-    expect(cli_lib_components.testFrame).to.be.a('string');
-    expect(cli_lib_components.testFrame).to.be.equal('jest');
+    expect(cli_components_react.testFrame).to.be.a('string');
+    expect(cli_components_react.testFrame).to.be.equal('jest');
 
-    expect(cli_lib_components.eslint).to.be.a('boolean');
-    expect(cli_lib_components.eslint).to.be.true;
+    expect(cli_components_react.eslint).to.be.a('boolean');
+    expect(cli_components_react.eslint).to.be.true;
 
-    expect(cli_lib_components.commitlint).to.be.a('boolean');
-    expect(cli_lib_components.commitlint).to.be.true;
+    expect(cli_components_react.commitlint).to.be.a('boolean');
+    expect(cli_components_react.commitlint).to.be.true;
 
-    expect(cli_lib_components.style).to.be.a('string');
-    expect(cli_lib_components.style).to.be.equal('less');
+    expect(cli_components_react.style).to.be.a('string');
+    expect(cli_components_react.style).to.be.equal('less');
 
-    expect(cli_lib_components.stylelint).to.be.a('boolean');
-    expect(cli_lib_components.stylelint).to.be.true;
+    expect(cli_components_react.stylelint).to.be.a('boolean');
+    expect(cli_components_react.stylelint).to.be.true;
 
-    expect(cli_lib_components.devServer).to.be.a('string');
-    expect(cli_lib_components.devServer).to.be.equal('docz');
+    expect(cli_components_react.devServer).to.be.a('string');
+    expect(cli_components_react.devServer).to.be.equal('docz');
   });
 
-  it('cli_lib_toolkit checking', function () {
-    expect(cli_lib_toolkit).to.be.an('object');
+  it('cli_toolkit checking', function () {
+    expect(cli_toolkit).to.be.an('object');
 
-    expect(cli_lib_toolkit.build).to.be.a('string');
-    expect(cli_lib_toolkit.build).to.be.equal('rollup');
+    expect(cli_toolkit.build).to.be.a('string');
+    expect(cli_toolkit.build).to.be.equal('rollup');
 
-    expect(cli_lib_toolkit.pkgtool).to.be.a('string');
-    expect(cli_lib_toolkit.pkgtool).to.be.equal('yarn');
+    expect(cli_toolkit.pkgtool).to.be.a('string');
+    expect(cli_toolkit.pkgtool).to.be.equal('yarn');
 
-    expect(cli_lib_toolkit['project_type']).to.be.a('string');
-    expect(cli_lib_toolkit['project_type']).to.be.equal('toolkit');
+    expect(cli_toolkit['project_type']).to.be.a('string');
+    expect(cli_toolkit['project_type']).to.be.equal('toolkit');
 
-    expect(cli_lib_toolkit.ts).to.be.a('boolean');
-    expect(cli_lib_toolkit.ts).to.be.true;
+    expect(cli_toolkit.ts).to.be.a('boolean');
+    expect(cli_toolkit.ts).to.be.true;
 
-    expect(cli_lib_toolkit.testFrame).to.be.a('string');
-    expect(cli_lib_toolkit.testFrame).to.be.equal('mocha');
+    expect(cli_toolkit.testFrame).to.be.a('string');
+    expect(cli_toolkit.testFrame).to.be.equal('mocha');
 
-    expect(cli_lib_toolkit.eslint).to.be.a('boolean');
-    expect(cli_lib_toolkit.eslint).to.be.true;
+    expect(cli_toolkit.eslint).to.be.a('boolean');
+    expect(cli_toolkit.eslint).to.be.true;
 
-    expect(cli_lib_toolkit.commitlint).to.be.a('boolean');
-    expect(cli_lib_toolkit.commitlint).to.be.true;
+    expect(cli_toolkit.commitlint).to.be.a('boolean');
+    expect(cli_toolkit.commitlint).to.be.true;
 
-    expect(cli_lib_toolkit.style).to.be.a('string');
-    expect(cli_lib_toolkit.style).to.be.equal('');
+    expect(cli_toolkit.style).to.be.a('string');
+    expect(cli_toolkit.style).to.be.equal('');
 
-    expect(cli_lib_toolkit.stylelint).to.be.a('boolean');
-    expect(cli_lib_toolkit.stylelint).to.be.false;
+    expect(cli_toolkit.stylelint).to.be.a('boolean');
+    expect(cli_toolkit.stylelint).to.be.false;
   });
 });
