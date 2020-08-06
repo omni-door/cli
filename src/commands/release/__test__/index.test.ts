faa384a8edf9d97383ca12e060dd7b57dde0e57c
bobby.li <bobby.lhan@gmail.com>
2020-08-06 22:51:29 +0800

    nyc exclude modify

--- src/commands/release/__test__/index.test.ts
+++ src/commands/release/__test__/index.test.ts
@@ -2,7 +2,7 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import release from '../index';
 
-describe('build command test', function () {
+describe('release command test', function () {
   it('type checking', function () {
     expect(release).to.be.a('function');
   });
